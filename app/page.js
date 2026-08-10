9cf849cca92448b1e1b364e13da2c29685312acf
foxevolta-tech <foxevolta@gmail.com>
2026-08-10 13:10:24 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Save the Date 💍</h1>
+      <p>Christian & Justine</p>
+      <p>16 Août 2026</p>
+    </div>
+  );
+}
